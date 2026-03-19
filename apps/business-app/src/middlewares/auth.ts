456a0ac0e433addd887d45949b70aabd0d9e6ad7
Tejas Ladhe <tejasladhe24@gmail.com>
2026-03-19 17:47:34 +0530

    correct auth flow

--- apps/business-app/src/middlewares/auth.ts
+++ apps/business-app/src/middlewares/auth.ts
@@ -1,33 +1,19 @@
-import { redirect } from "@tanstack/react-router"
 import { createMiddleware } from "@tanstack/react-start"
 import { getRequestHeaders } from "@tanstack/react-start/server"
 import { authClient } from "@/lib/auth-client"
 
 export const authMiddleware = createMiddleware({ type: "request" }).server(
-  async ({ next, request }) => {
+  async ({ next }) => {
     const { data } = await authClient.getSession({
       fetchOptions: {
         headers: getRequestHeaders(),
       },
     })
 
-    if (!data?.session || !data?.user) {
-      throw redirect({ to: "/login" })
-    }
-
-    const url = new URL(request.url)
-
-    if (
-      !data?.session?.activeOrganizationId &&
-      !url.pathname.startsWith("/select-org")
-    ) {
-      throw redirect({ to: "/select-org" })
-    }
-
     return await next({
       context: {
-        session: data.session,
-        user: data.user,
+        session: data?.session,
+        user: data?.user,
       },
     })
   }
